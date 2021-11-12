1d3ab2c7cce52d5b392a65048b98f2280adb0ae8
firmino_neto <firminoneto11@hotmail.com>
2021-11-12 14:19:03 -0300

    Implemented a fully featured login page with MUI.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,20 +1,58 @@
 
 import AuthContext from "../context/AuthContext";
-import { Fragment } from "react";
 import { useContext } from "react";
+import Copyright from '../components/Copyright';
 
+// MUI Imports
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 export default function LoginPage() {
 
     const { logIn } = useContext(AuthContext);
 
     return (
-        <Fragment>
-            <form onSubmit={logIn}>
-                <input type="text" name="email" placeholder="Enter your e-mail" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
-        </Fragment>
-    );
+        <ThemeProvider theme={theme}>
+          <Grid container component="main" sx={{ height: '100vh' }}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} sx={{
+                backgroundImage: 'url(https://source.unsplash.com/random)',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: (t) =>
+                  t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+              <Box sx={{
+                  my: 8,
+                  mx: 4,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} />
+                <Typography component="h1" variant="h5"> Sign in</Typography>
+                <Box component="form" autoComplete="off" noValidate onSubmit={logIn} sx={{ mt: 1 }}>
+                  <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" 
+                  autoComplete="email" autoFocus />
+                  <TextField margin="normal" required fullWidth name="password" label="Password" type="password" 
+                  id="password" autoComplete="current-password" />
+                  <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign In</Button>
+                  <Copyright sx={{ mt: 5 }} />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </ThemeProvider>
+      );
 }
